fb1a2909959969a2aa7af67dd869cba48de983fa
Roger Torres <torresroger445@gmail.com>
2023-09-22 19:00:29 -0600

    feat: :zap: adding platform version with method channel

--- android/app/src/main/kotlin/com/example/challenge/MainActivity.kt
+++ android/app/src/main/kotlin/com/example/challenge/MainActivity.kt
@@ -1,19 +1,32 @@
 package com.example.challenge
 
+import android.os.Build
 import io.flutter.embedding.android.FlutterActivity
 import io.flutter.embedding.engine.FlutterEngine
 import io.flutter.plugin.common.MethodCall
 import io.flutter.plugin.common.MethodChannel
 
 class MainActivity: FlutterActivity() {
-    private val CHANNEL = "com.example/data_channel"
+    private val PLATFORM_VERSION_CHANNEL = "com.example/platform_version"
+    private val DATA_CHANNEL = "com.example/data_channel"
 
     override fun configureFlutterEngine(flutterEngine: FlutterEngine) {
         super.configureFlutterEngine(flutterEngine)
+        // Canal para obtener la versión de la plataforma
+        val platformVersionChannel = MethodChannel(flutterEngine.dartExecutor.binaryMessenger, PLATFORM_VERSION_CHANNEL)
 
-        val channel = MethodChannel(flutterEngine.dartExecutor.binaryMessenger, CHANNEL)
+        platformVersionChannel.setMethodCallHandler { call: MethodCall, result: MethodChannel.Result ->
+            if (call.method == "getPlatformVersion") {
+                val platformVersion = "Android ${Build.VERSION.RELEASE}"
+                result.success(platformVersion)
+            } else {
+                result.notImplemented()
+            }
+        }
+        // Canal para la comunicación de datos ("dataChannel")
+        val dataChannel = MethodChannel(flutterEngine.dartExecutor.binaryMessenger, DATA_CHANNEL)
 
-        channel.setMethodCallHandler { call: MethodCall, result: MethodChannel.Result ->
+        dataChannel.setMethodCallHandler { call: MethodCall, result: MethodChannel.Result ->
             if (call.method == "concatenateString") {
                 if (call.arguments is String) {
                     val messageFromFlutter = call.arguments as String
